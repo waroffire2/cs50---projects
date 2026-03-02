528e1a4e8212b681b3809b567999b1d8919f28fa
Arton dz <161343584+waroffire2@users.noreply.github.com>
2026-03-02 17:47:40 -0500

    Add files via upload

---
+++ Week 4 Problem Set/volume.c
@@ -0,0 +1,58 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    // Read the header and store it then write it to output
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    // Create int16_t variable that will keep storing a sample, note: we have to use &
+    // fread(. . .) == 1 because we are checking only for 1 found item/sample, o.w. 0
+    // Update buffer with factor and write back to output
+
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
+    {
+        buffer *= factor;
+
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
